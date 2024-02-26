11ae8fa2d6098862a660a89188d141890e4a9379
Nathan H <nathan0704@icloud.com>
2024-02-26 19:37:12 +0000

    did a bit of tidying up the layout

---
+++ Pages/LogIn.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FarmApplication.Pages
+{
+    public class LogInModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
