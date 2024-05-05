fbbd348da0262bc595d34498d7f1f66d65718ac7
Ell098 <ewright301@yahoo.com>
2024-05-05 19:44:52 +0100

    Changed web page to add map, updated css and clock positioning. chnaged name displayed for the app name. added map databse to be implemented

---
+++ Model/Map.cs
@@ -0,0 +1,30 @@
+﻿using FarmApplication.Areas.Identity.Data;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FarmApplication.Model
+{
+	public class Map
+	{
+		[Key]
+		public int FarmID { get; set; }
+
+		[Required]
+
+		public float Latitute1 { get; set; }
+		public float Latitute2 { get; set; }
+		public float Latitute3 { get; set; }
+		public float Latitute4 { get; set; }
+
+		public float Longitude1 { get; set; }
+		public float Longitude2 { get; set; }
+		public float Longitude3 { get; set; }	
+		public float Longitude4 { get; set; }
+
+		public string FieldID { get; set; }
+		[ForeignKey("FieldID")]
+
+        public virtual FarmApplicationDBUser AspNetUsers { get; set; }
+
+    }
+}
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,30 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function initMap() {
+    var uk = { lat: 53.290, lng: -1.368 }
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 8,
+        center: uk,
+        mapTypeId: "terrain",
+    });
+    const field = [
+        { lat: 53.0538, lng: 0.0640 },
+        { lat: 53.0533, lng: 0.0641 },
+        { lat: 53.0534, lng: 0.0652 },
+        { lat: 53.0538, lng: 0.0652 },
+    ];
+    const fieldArea = new google.maps.Polygon({
+        path: field,
+        geodesic: true,
+        strokeColor: "Blue",
+        strokeOpacity: 1.0,
+        strokeWeight: 2,
+    });
+
+    fieldArea.setMap(map);
+}
+
+window.initMap = initMap;
+
